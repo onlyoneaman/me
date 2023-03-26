4216b508b1956115e161ec4efb1b78ce3d926478
Aman <2000.aman.sinha@gmail.com>
2023-03-26 21:13:59 +0530

    Remove Resume | Update Nav Icon

--- src/components/nav.js
+++ src/components/nav.js
@@ -173,9 +173,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-      Resume
-    </a>
+    <div>
+      {/*<a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">*/}
+      {/*  Resume*/}
+      {/*</a>*/}
+    </div>
   );
 
   return (
